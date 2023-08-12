cf3406d25b1e5e9f6bbaa33b9c6db374f582985b
davidpinhas <davepinhas89@gmail.com>
2023-08-12 15:56:22 +0300

    improve prepare-env.sh script

--- chapters/chapter1-create-your-kubernetes-cluster/scripts/prepare-env.sh
+++ chapters/chapter1-create-your-kubernetes-cluster/scripts/prepare-env.sh
@@ -4,70 +4,73 @@ echo "###########################################
 #### Road To DevOps Preperation Script ####
 ###########################################"
 
-# Verify if running on macOS
-if [ "$(uname)" != "Darwin" ]; then
-    echo "Error: This script is intended to run on macOS only."
+# Check if the script is running on macOS
+if [[ $(uname -s) != "Darwin" ]]; then
+    echo "ERROR: This script only works on macOS."
     exit 1
 fi
 
 # Create directories
-echo "### INFO: Creating Directories."
+echo "INFO: Creating Directories."
 mkdir -p ~/.oci
 mkdir -p $HOME/workspace/cloud
 
 # Create RSA key
-echo "### INFO: Generating RSA keys under ~/.oci directory."
+echo "INFO: Generating RSA keys under ~/.oci directory."
 openssl genrsa -out ~/.oci/${USER}-oracle-cloud.pem 4096
 chmod 600 ~/.oci/${USER}-oracle-cloud.pem
 openssl rsa -pubout -in ~/.oci/${USER}-oracle-cloud.pem -out ~/.oci/${USER}-oracle-cloud_public.pem
 
 # Clone GitHub K3s repository
-echo "### INFO: Cloning K3s repository to $HOME/workspace/cloud."
+echo "INFO: Cloning K3s repository to $HOME/workspace/cloud."
+current_dir=$(pwd)
 cd $HOME/workspace/cloud
 git clone https://github.com/garutilorenzo/k3s-oci-cluster.git
-cd
+cd $current_dir
 
 # Install Prerequisites
-echo "##################################"
-echo "#### Installing Prerequisites ####"
-echo "##################################"
+echo "
+##################################
+#### Installing Prerequisites ####
+##################################"
+
+# Install Homebrew
 if ! command -v brew; then
-    echo "### INFO: Installing Homebrew."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/daveops/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-    echo "### INFO: Homebrew installed successfully."
-else
-    echo "### INFO: Homebrew is already installed."
-fi
-
-if ! command -v terraform; then
-    echo "### INFO: Installing Terraform."
+    echo "INFO: Installing Homebrew."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
     brew update
-    brew tap hashicorp/tap
-    brew install hashicorp/tap/terraform
-    echo "### INFO: Terraform installed successfully."
+    echo "INFO: Homebrew installed successfully."
 else
-    echo "### INFO: Terraform is already installed."
+    echo "INFO: Homebrew is already installed."
 fi
 
-if ! command -v python3.10; then
-    echo "### INFO: Installing Python 3.10."
-    brew install python@3.10
-    echo "### INFO: Python 3.10 installed successfully."
-else
-    echo "### INFO: Python 3.10 is already installed."
-fi
+# Function to install a tool using Homebrew
+install_with_brew() {
+    tool_name="$1"
+    install_command="$2"
+    already_installed_message="$3"
 
-if ! command -v oci; then
-    echo "### INFO: Installing OCI CLI."
-    brew update
-    brew install oci-cli
-    echo "### INFO: OCI CLI installed successfully."
-else
-    echo "### INFO: OCI CLI is already installed."
-fi
+    if ! command -v "$tool_name"; then
+        echo "INFO: Installing $tool_name."
+        if brew install "$install_command"; then
+            echo "INFO: $tool_name installed successfully."
+        else
+            echo "ERROR: Failed to install $tool_name."
+            exit 1
+        fi
+    else
+        echo "INFO: $already_installed_message."
+    fi
+}
 
-echo "###########################################"
-echo "#### Environment preparation completed ####"
-echo "###########################################"
+# Install CLIs
+install_with_brew "brew" "" "Homebrew is already installed." # Install Homebrew if not installed
+install_with_brew "terraform" "hashicorp/tap/terraform" "Terraform is already installed." # Install Terraform
+install_with_brew "python3.10" "python@3.10" "Python 3.10 is already installed." # Install Python 3.10
+install_with_brew "oci" "oci-cli" "OCI CLI is already installed." # Install OCI CLI
+install_with_brew "kubectl" "" "kubectl is already installed." # Install kubectl
+
+echo "
+###########################################
+#### Environment preparation completed ####
+###########################################"
